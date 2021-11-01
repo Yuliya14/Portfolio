bbc46639e921ee895ab5326b7c579950656ad281
Yuliya <julia1997.04.14@mail.ru>
2021-11-02 00:02:43 +0300

    refactoring and change Portfolio module

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,50 +1,31 @@
 import React from 'react'
-import style from './Skills.module.css'
+import style from './Skills2.module.css'
 import styleContainer from './../common/styles/Container.module.css'
-export const Skills = () => {
-    return <div></div>
-}
-/*
-import {SkillsType} from "../App";
-type SkillsPropsType = {
-    skills: Array<SkillsType>
-}
+import {Skill} from "../skill/Skill";
 
-export const Skills = (props: SkillsPropsType) => {
-    return <div className={style.skills}>
-        <div className={`${styleContainer.container} ${style.containerSkills}`}>
-            <div className={style.skillsInner}>
+export const Skills2 = () => {
+    return (
+        <div className={style.skills}>
+            <div className={`${styleContainer.container} ${style.containerSkills}`}>
+
                 <div className={style.skillsTitle}>
                     <span>Skills</span>
                     <h3>Programming Skills</h3>
                 </div>
+
                 <div className={style.skillsText}>
                     <p>Any fool can work with intelligence in IT, you try without it!</p>
                 </div>
-                <div>{
-                    props.skills.map(sk => {
-                        return <div className={style.skillsProgress} key={sk.id}>
-                            <div className={style.progressInner}>
-                    <span>
-                        <span className={style.label}>{sk.name}</span>
-                        <span className={style.number}>{sk.progress}</span>
-                    </span>
-                                <div className={style.background}>
-                                    <div className={style.barOpen}>
-                                        <div className={style.barIn}></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    })
-                }
-                </div>
-                <div className = {style.images}>
-                   <img src={"https://freeiconshop.com/wp-content/uploads/edd/html-outline.png"}/>
+
+                <div className={style.skillsBlock}>
+                    <Skill title={"HTML && CSS"}
+                           description = {" HTML (the Hypertext Markup Language) and CSS (Cascading Style Sheets) are two of the core technologies for building Web pages"}/>
+                    <Skill title={"JS"} description = {"blabla"}/>
+                    <Skill title={"React"} description = {"yoyo"}/>
+                    <Skill title={"Redux"} description = {"dada"}/>
+
                 </div>
             </div>
 
-        </div>
-    </div>
+        </div>)
 }
-*/
